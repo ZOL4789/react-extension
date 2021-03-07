09eeacdfa112b5833209f49b964bb93977b15a07
RandomPlayer <185729226@qq.com>
2021-03-07 11:25:31 +0800

    增加学习

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
-import Demo from './components/1_setState'
+import SetState from './components/1_setState'
+import LazyLoad from './components/2_lazyLoad'
+
+
+
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Demo/>
+                <div className="container">
+                    <SetState something={100}/>
+                </div>
+                <div>
+                    <LazyLoad/>
+                </div>
             </div>
         )
     }
--- src/components/1_setState/index.jsx
+++ src/components/1_setState/index.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
 
-export default class Demo extends Component {
+export default class SetState extends Component {
 
     state = {sum:0}
 
     add = ()=>{
-        const {sum} = this.state;
-        // 异步render
-        this.setState({sum: sum + 1}, ()=>{
-            // 更新了sum是+1之后的值
-            console.log(this.state.sum);
-        });
-        // 更新完了sum还是没+1的值
+        //const {sum} = this.state;
+        // // 异步render
+        // this.setState({sum: sum + 1}, ()=>{
+        //     // 更新了sum是+1之后的值
+        //     console.log(this.state.sum);
+        // });
+        // // 更新完了sum还是没+1的值
+        // console.log(this.state.sum);
+        
+        // 函数式setState
+        this.setState((state, props)=>{
+            console.log(state);
+            console.log(props);
+            return {sum:state.sum+1}
+        })
         console.log(this.state.sum);
     }
 
